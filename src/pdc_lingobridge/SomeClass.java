fa88229a9ddbd442253d8d1d2e08f97ddf5cd1dc
SamHeuss <nps6648@autuni.ac.nz>
2023-10-10 11:38:51 +1300

    somethign

---
+++ src/pdc_lingobridge/SomeClass.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pdc_lingobridge;
+
+/**
+ *
+ * @author sammy
+ */
+public class SomeClass {
+    public static void main(String[] args) {
+        System.out.println("TEST");
+    }
+}
