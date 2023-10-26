1b6ee00b62d6d7bf8f723d073b0c792e0d7c1b25
SamHeuss <nps6648@autuni.ac.nz>
2023-10-26 15:48:58 +1300

    Design PlayGame

--- src/pdc_lingobridge/Homepage.java
+++ src/pdc_lingobridge/Homepage.java
@@ -4,22 +4,11 @@
  */
 package pdc_lingobridge;
 
-import java.awt.Color;
-import java.awt.Cursor;
-import java.awt.Font;
-import java.awt.event.*;
-
-import javax.swing.GroupLayout;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.LayoutStyle;
+import javax.swing.*;
+import java.awt.*;
 import javax.swing.border.LineBorder;
 
 /**
- *
  * @author celin
  */
 public class Homepage extends JPanel {
@@ -31,7 +20,8 @@ public class Homepage extends JPanel {
     private JLabel title;
     private JLabel titleEnterUser;
     private JTextField usernameInput;
-    
+    private DeckSelectionTest deckSelectionPanel; // The deck selection panel
+
     public Homepage() {
         this.initComponents();
     }
@@ -44,79 +34,76 @@ public class Homepage extends JPanel {
         titleEnterUser = new JLabel();
         instructionButton = new JButton();
         leaderButton = new JButton();
+        deckSelectionPanel = new DeckSelectionTest(); // Initialize the deck selection panel
 
         jMenu1.setText("jMenu1");
 
         setBackground(new Color(203, 226, 247));
 
         usernameInput.setBackground(new Color(246, 247, 249));
-        usernameInput.setFont(new Font("Dialog", 0, 18)); // NOI18N
+        usernameInput.setFont(new Font("Dialog", 0, 18));
         usernameInput.setForeground(new Color(51, 51, 51));
         usernameInput.setHorizontalAlignment(JTextField.CENTER);
-        usernameInput.setToolTipText("");
         usernameInput.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
         usernameInput.setCursor(new Cursor(Cursor.TEXT_CURSOR));
-        usernameInput.setSelectedTextColor(new Color(0, 0, 0));
 
-        title.setBackground(new Color(255, 255, 255));
-        title.setFont(new Font("MV Boli", 1, 48)); // NOI18N
+        title.setFont(new Font("MV Boli", 1, 48));
         title.setText("Lingo Bridge");
 
         confirmButton.setBackground(new Color(204, 226, 165));
-        confirmButton.setFont(new Font("Dialog", 0, 12)); // NOI18N
+        confirmButton.setFont(new Font("Dialog", 0, 12));
         confirmButton.setText("Confirm");
-        confirmButton.addActionListener((ActionEvent evt) -> {
-            LingoBridge.getInstance().confirmButtonActionPerformed(usernameInput, evt);
+        confirmButton.addActionListener(evt -> {
+            // LingoBridge.getInstance().confirmButtonActionPerformed(usernameInput, evt);
+            System.out.println("Confirm button pressed"); // Placeholder code
         });
 
-        titleEnterUser.setFont(new Font("Dialog", 0, 12)); // NOI18N
+        titleEnterUser.setFont(new Font("Dialog", 0, 12));
         titleEnterUser.setText("Enter your Username");
 
         instructionButton.setBackground(new Color(204, 204, 255));
-        instructionButton.setFont(new Font("Dialog", 0, 12)); // NOI18N
+        instructionButton.setFont(new Font("Dialog", 0, 12));
         instructionButton.setText("How to Play");
-        instructionButton.addActionListener((ActionEvent evt) -> {
-            LingoBridge.getInstance().instructionButtonActionPerformed(evt);
+        instructionButton.addActionListener(evt -> {
+            // LingoBridge.getInstance().instructionButtonActionPerformed(evt);
+            System.out.println("Instructions button pressed"); // Placeholder code
         });
 
         leaderButton.setBackground(new Color(204, 204, 255));
-        leaderButton.setFont(new Font("Dialog", 0, 12)); // NOI18N
+        leaderButton.setFont(new Font("Dialog", 0, 12));
         leaderButton.setText("Leaderboard");
-        leaderButton.addActionListener((ActionEvent evt) -> {
-            LingoBridge.getInstance().leaderButtonActionPerformed(evt);
+        leaderButton.addActionListener(evt -> {
+            // LingoBridge.getInstance().leaderButtonActionPerformed(evt);
+            System.out.println("Leaderboard button pressed"); // Placeholder code
         });
 
         GroupLayout mainPanelLayout = new GroupLayout(this);
         this.setLayout(mainPanelLayout);
         mainPanelLayout.setHorizontalGroup(
                 mainPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addGroup(GroupLayout.Alignment.TRAILING, mainPanelLayout.createSequentialGroup()
-                                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGroup(mainPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                        .addGroup(GroupLayout.Alignment.TRAILING, mainPanelLayout.createSequentialGroup()
-                                                .addComponent(confirmButton)
-                                                .addGap(328, 328, 328))
-                                        .addGroup(GroupLayout.Alignment.TRAILING, mainPanelLayout.createSequentialGroup()
-                                                .addComponent(titleEnterUser)
-                                                .addGap(311, 311, 311))))
                         .addGroup(mainPanelLayout.createSequentialGroup()
                                 .addGap(260, 260, 260)
-                                .addComponent(usernameInput, GroupLayout.PREFERRED_SIZE, 218, GroupLayout.PREFERRED_SIZE)
-                                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addGroup(GroupLayout.Alignment.TRAILING, mainPanelLayout.createSequentialGroup()
-                                .addGap(0, 223, Short.MAX_VALUE)
-                                .addGroup(mainPanelLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                                .addComponent(usernameInput, GroupLayout.PREFERRED_SIZE, 218, GroupLayout.PREFERRED_SIZE))
+                        .addGroup(mainPanelLayout.createSequentialGroup()
+                                .addGap(223, 223, 223)
+                                .addGroup(mainPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                                         .addGroup(mainPanelLayout.createSequentialGroup()
                                                 .addComponent(instructionButton)
                                                 .addGap(113, 113, 113)
                                                 .addComponent(leaderButton))
-                                        .addComponent(title))
-                                .addGap(210, 210, 210))
+                                        .addComponent(title)
+                                        .addComponent(deckSelectionPanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))) // Add the deck selection panel here
+                        .addGroup(mainPanelLayout.createSequentialGroup()
+                                .addGap(328, 328, 328)
+                                .addComponent(confirmButton))
+                        .addGroup(mainPanelLayout.createSequentialGroup()
+                                .addGap(311, 311, 311)
+                                .addComponent(titleEnterUser))
         );
         mainPanelLayout.setVerticalGroup(
                 mainPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addGroup(GroupLayout.Alignment.TRAILING, mainPanelLayout.createSequentialGroup()
-                                .addContainerGap(51, Short.MAX_VALUE)
+                        .addGroup(mainPanelLayout.createSequentialGroup()
+                                .addGap(51, 51, 51)
                                 .addComponent(title)
                                 .addGap(18, 18, 18)
                                 .addComponent(titleEnterUser)
@@ -124,11 +111,22 @@ public class Homepage extends JPanel {
                                 .addComponent(usernameInput, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(confirmButton)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(deckSelectionPanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE) // And in the vertical layout
                                 .addGap(46, 46, 46)
                                 .addGroup(mainPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                                         .addComponent(leaderButton)
                                         .addComponent(instructionButton))
-                                .addGap(118, 118, 118))
+                                .addContainerGap(50, Short.MAX_VALUE))
         );
     }
+
+    public static void main(String[] args) { // Test
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new Homepage());
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
+
